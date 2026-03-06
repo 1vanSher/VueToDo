59476b7125d836b201bc5b6fe9a1a40a97b6a71b
1vanSher <ivan.sher.05@mail.ru>
2026-03-06 20:18:55 +0300

    feat: delete todo add

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import {ref, watch, reactive} from 'vue'
+
+const todos = ref([
+  {
+    title: 'Хлеб',
+    id: 1,
+    complited: true
+  },
+  {
+    title: 'Молоко',
+    id: 2,
+    complited: true
+  },
+  {
+    title: 'Вода',
+    id: 3,
+    complited: false
+  },
+  {
+    title: 'Чипсы',
+    id: 4,
+    complited: false
+  }
+])
+
+const deleteTodo = (id) =>{
+  todos.value = todos.value.filter(todo => todo.id !== id)
+}
+
+watch(todos, () =>{
+  console.log(todos);
+},
+{
+  deep:true
+}
+)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div v-for="todo in todos" :key="todo.id">
+      <h2>{{ todo.title }}</h2>
+      <input v-model="todo.complited" type="checkbox">
+      <button @click="deleteTodo(todo.id)">Delete</button>
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
