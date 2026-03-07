3b4c80eeec346014e1277786b4726b7b1b279df1
1vanSher <ivan.sher.05@mail.ru>
2026-03-07 19:26:52 +0300

    feat/version with styles

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import AddTodo from './components/addTodo.vue'
 
 const todos = ref([
   {
-    title: 'Хлеб',
+    title: 'Купить хлеб и изучить Vue.js',
     id: 1,
     completed: true
   },
@@ -43,11 +43,13 @@ const allTodosDone = computed(() =>{
 })
 
 const allTodosNotDone = computed(() =>{
-  console.log(newTodos.value.length === todos.value.length)
   return newTodos.value.length === todos.value.length
 })
 
 const addToDo = (text) =>{
+  if(text === ''){
+    return
+  }
   todos.value.push({title: text, id: todos.value.length+1, completed: false})
 }
 
@@ -66,11 +68,24 @@ watch(todos, () =>{
     <h2 v-if="allTodosDone">Все задачи выполнены</h2>
     <h2 v-else>Tasks to do {{ newTodos.length }}</h2>
     <TodoItem v-for="todo in newTodos" :key="todo.id" :todo="todo" @deleteTodo="deleteTodo"/>
-    <hr>
-    <h2 v-if="allTodosNotDone">done <br>Ничего не найдено</h2>
-    <h2 v-else>done - {{ doneTodos.length }}</h2>
+    <h2 v-if="allTodosNotDone">Done <br>Ничего не найдено...</h2>
+    <h2 v-else>Done - {{ doneTodos.length }}</h2>
     <TodoItem v-for="todo in doneTodos" :key="todo.id" :todo="todo" @deleteTodo="deleteTodo"/>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+  main{
+    width: 583px;
+    min-height: 758px;
+    background-color: #1D1825;
+    color: #ffffff;
+    padding: 50px 86px 65px 55px;
+    border-radius: 20px;
+    font-size: 12px;
+  }
+  h2{
+    margin-top: 60px;
+    margin-bottom: 15px;
+  }
+</style>
--- src/components/addTodo.vue
+++ src/components/addTodo.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ref} from 'vue'
+import IconCommunity from './icons/IconCommunity.vue'
 
 const inputValue = ref('')
 
@@ -11,6 +12,33 @@ const addToDo = () =>{
 }
 </script>
 <template>
-    <input v-model="inputValue" type="text">
-    <button @click="addToDo">+</button>
+    <div>
+        <input v-model="inputValue" type="text" placeholder="Add a new task">
+        <button @click="addToDo"><IconCommunity/></button>
+    </div>
 </template>
+
+<style scoped>
+    div{
+        width: 432px;
+        display:  flex;
+        justify-content: space-between;
+    }
+    input{
+        outline: none;
+        font-size: 16px;
+        font-weight: 400;
+        padding-left: 15px;
+        width: 381px;
+        height: 40px;
+        border: solid 2px #9E78CF;
+        border-radius: 10px;
+    }
+    button{
+        width: 40px;
+        height: 40px;
+        border-radius: 10px;
+        border: none;
+        background-color: #9E78CF;
+    }
+</style>
